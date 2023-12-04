f9d8b35b3363c390b4045131c4616d224b9b6f0a
Sebastian Heil <mail+git@sebastianheil.de>
2023-12-04 22:17:13 +0100

    Day04: Initial solution

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,16 +1,36 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun String.removeRegexPrefix(regex: Regex) = replace(regex, "")
+    fun Int.pow(exp: Int) = toBigInteger().pow(exp).toInt()
+
+    fun matchingWinningNumbers(line: String): Int {
+        val (winnerSet, ownSet) = line
+            .removeRegexPrefix("Card \\d+:".toRegex())
+            .replace("  ", " ")
+            .split("|")
+            .map { it.trim().split(" ") }
+        return ownSet.count { it in winnerSet }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int = input.sumOf { line ->
+        val count = matchingWinningNumbers(line)
+        if (count == 0) 0
+        else 2.pow(count - 1)
     }
+
+    fun part2(input: List<String>): Int =
+        input.withIndex().fold(MutableList(input.size) { 1 }) { list, (cardId, line) ->
+            val count = matchingWinningNumbers(line)
+            for (offset in 1..count) {
+                val cards = list[cardId]
+                list[cardId + offset] = list[cardId + offset] + cards
+            }
+            list
+        }.sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     part1(input).println()
