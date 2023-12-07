b06bdc985f41e0416ab7ddcbfa6cdfbff266d57c
Sebastian Heil <mail+git@sebastianheil.de>
2023-12-07 07:50:44 +0100

    Add Day08 template

---
+++ src/Day08.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day08_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day08")
+    part1(input).println()
+    part2(input).println()
+}
