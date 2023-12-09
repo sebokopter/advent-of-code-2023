362bcf1b5d5eaad24658ee756434bc28306706e5
Sebastian Heil <mail+git@sebastianheil.de>
2023-12-09 10:01:25 +0100

    Day07: Ugly solution

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,53 +1,45 @@
-import kotlin.math.abs
-
 fun main() {
-    fun differences(input: List<Int>): MutableList<MutableList<Int>> {
+    fun differences(input: List<Long>): MutableList<MutableList<Long>> {
         val differences = mutableListOf(input.toMutableList())
         do {
             val newDifference = differences.last().windowed(2, 1) { (a, b) ->
-                abs(b - a)
+                b - a
             }
             differences.add(newDifference.toMutableList())
-        } while (!differences.last().all { it == 0 })
+        } while (!differences.last().all { it == 0L })
         return differences
     }
 
-    fun extrapolate(input: List<Int>): Int {
+    fun extrapolate(input: List<Long>, extrapolateBeginning: Boolean = false): Long {
         val differences = differences(input)
-        println(differences)
-        val reversed: MutableList<MutableList<Int>> = differences.reversed().toMutableList()
-
+        val reversed: MutableList<MutableList<Long>> = differences.reversed().toMutableList()
         reversed.first().add(0)
-        var last = 0
-        println(reversed)
+        var extrapolatedItem = 0.toLong()
         reversed.windowed(2, 1) { (a, b) ->
-            if (a.isEmpty()) println(a.last())
-            if (b.isEmpty()) println(b.last())
-            last = a.last() + b.last()
-            b.add(last)
+            extrapolatedItem = if (extrapolateBeginning) b.first() - a.first() else a.last() + b.last()
+            val res = if (extrapolateBeginning) mutableListOf(extrapolatedItem) + b else b + extrapolatedItem
+            b.clear()
+            b.addAll(res)
         }
-        return last
+        return extrapolatedItem
     }
 
-    fun part1(input: List<String>): Int {
-        val histories = input.map { line ->
-            line.split(" ").map { it.toInt() }
-        }
-        return histories.sumOf {
-            extrapolate(it)
-        }
+    fun part1(input: List<String>): Long {
+        val histories = input.map { line -> line.split(" ").map { it.toLong() } }
+        return histories.sumOf(::extrapolate)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val histories = input.map { line -> line.split(" ").map { it.toLong() } }
+        return histories.sumOf { extrapolate(it, true) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 114)
-//    check(part2(testInput) == 0)
+    check(part1(testInput) == 114L)
+    check(part2(testInput) == 2L)
 
     val input = readInput("Day09")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
